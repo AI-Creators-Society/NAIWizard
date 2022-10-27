ad393acc60d6bc4461a355077bfb9b7cf283afc4
unknown <git@p1at.dev>
2022-10-27 13:43:18 +0900

    chore: nextui init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { createTheme, NextUIProvider } from "@nextui-org/react"
+
+const darkTheme = createTheme({
+    type: "dark",
+    theme: {
+        colors: {
+            background: "#171F26",
+            secondaryBackground: "#1D242B",
+            text: "#fff",
+            accent: "#F3BB59",
+        },
+        space: {},
+        fonts: {},
+    },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <NextUIProvider theme={darkTheme}>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    )
 }
