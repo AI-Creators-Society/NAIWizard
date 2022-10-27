e8ee0b4147c273de780de38abcec6e50f09eee00
unknown <git@p1at.dev>
2022-10-27 13:55:42 +0900

    chore: bye nextui, hello chakraui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,20 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import { createTheme, NextUIProvider } from "@nextui-org/react"
 
-const darkTheme = createTheme({
-    type: "dark",
-    theme: {
-        colors: {
-            background: "#171F26",
-            secondaryBackground: "#1D242B",
-            text: "#fff",
-            accent: "#F3BB59",
-        },
-        space: {},
-        fonts: {},
-    },
-})
+// const darkTheme = createTheme({
+//     type: "dark",
+//     theme: {
+//         colors: {
+//             background: "#171F26",
+//             secondaryBackground: "#1D242B",
+//             text: "#fff",
+//             accent: "#F3BB59",
+//         },
+//         space: {},
+//         fonts: {},
+//     },
+// })
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <NextUIProvider theme={darkTheme}>
-            <Component {...pageProps} />
-        </NextUIProvider>
-    )
+    return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
-import { Card, Container, Row, Text } from "@nextui-org/react"
 import Head from "next/head"
 import Image from "next/image"
 
 export default function Home() {
-    return (
-        <>
-            <Container xl>
-                <Text>Hello</Text>
-            </Container>
-        </>
-    )
+    return <></>
 }
