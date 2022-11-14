52d2e745e2f97ff87469d9b01dec46e49ff7a8fa
unknown <git@p1at.dev>
2022-11-15 03:19:11 +0900

    support ogp

---
+++ src/utils/headData.ts
@@ -0,0 +1,13 @@
+export default {
+    title: "NAIWizard",
+    description: "NovelAI 呪文構築支援機",
+    canonical: "https://naiwizard.p1at.dev",
+    favicon: "/favicon.ico",
+    og: {
+        image: "/naiwizard_ogp.png",
+    },
+    twitter: {
+        handle: "@p1atdev",
+        cardType: "summary_large_image",
+    },
+}
